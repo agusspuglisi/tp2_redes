478f77366055678542b721c434a7e09be3468b21
FranRodriguezz <frrodriguez@fi.uba.ar>
2025-06-22 19:48:19 -0300

    Agrego topologia parametrizable

--- topo/topologia.py
+++ topo/topologia.py
@@ -1,14 +1,23 @@
-# Archivo: topo/topologia.py
 from mininet.topo import Topo
 
-class BasicTopo(Topo):
-    def build(self):
-        s1 = self.addSwitch('s1')
+class ChainTopo(Topo):
+    def build(self, n=3):
+        if n < 1:
+            raise Exception("La cantidad de switches debe ser >= 1")
 
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
 
-        self.addLink(h1, s1)
-        self.addLink(h2, s1)
+        switches = []
+        for i in range(n):
+            switch = self.addSwitch(f's{i+1}')
+            switches.append(switch)
 
-topos = {'basic': BasicTopo}
+        self.addLink(h1, switches[0])
+
+        for i in range(n - 1):
+            self.addLink(switches[i], switches[i + 1])
+
+        self.addLink(h2, switches[-1])
+
+topos = { 'chain': ChainTopo }
